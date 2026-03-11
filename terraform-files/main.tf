1abfa6123325eb685dbbb7c86e4387230d1d9b1f
Pratap Mulakala <mulakalapratap777@gmail.com>
2026-03-11 16:15:51 +0530

    Change instance type from t2.micro to t3.micro

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test-server" {
   ami = "ami-0b6c6ebed2801a5cb"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name = "key"
   subnet_id = "subnet-04103d4f13bfa451c"
   vpc_security_group_ids = ["sg-0178d50f27e5cf78c"]
