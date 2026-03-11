2dd3a2c8fd3ca61fe1d5457a2d45d0bb065a08ac
Pratap Mulakala <mulakalapratap777@gmail.com>
2026-03-11 15:55:38 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-0360c520857e3138f"
+  ami = "ami-0b6c6ebed2801a5cb"
   instance_type = "t2.micro"
-  key_name = "sir"
-  vpc_security_group_ids = ["sg-08348db6dca8c5940"]
+  key_name = "key"
+  vpc_security_group_ids = ["sg-0178d50f27e5cf78c"]
   connection {
      type = "ssh"
      user = "ubuntu"
-     private_key = file("./sir.pem")
+     private_key = file("./key.pem")
      host = self.public_ip
      }
   provisioner "remote-exec" {
